a4d4a40052a689420426b9e2826b3b91488e8b6f
Adarsh V <adarshv41@gmail.com>
2021-07-31 17:44:26 +0530

    react redux

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import {useSelector} from 'react-redux'
+
+function Header() {
+  const color=useSelector(state=> state.color.value)
+  const count=useSelector(state => state.count.value)
+  return (
+    <div className="header">
+      <h1 style={{color}}>REDUX Project</h1>
+      <h3>The count is {count}</h3>
+    
+    </div>
+  );
+}
+
+export default Header;
